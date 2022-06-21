a20a9f60c656d5ca22b07c05b9998f4a80f28b7c
garfo1986 <97093739+garfo1986@users.noreply.github.com>
2022-06-21 11:19:25 +0200

    Add files via upload

---
+++ triangle&circle.py
@@ -0,0 +1,19 @@
+
+
+
+
+def Triangle_area(base,high):
+    print (f"triangle's area is {(base*high)/2}")
+
+def Circle_area(radio_number):
+    pi_number=3.1416
+
+    print(f"circle's area is {(pi_number*radio_number)}")
+
+
+Triangle_area(6, 8)
+
+Circle_area(42)
+
+
+        
